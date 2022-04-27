f8de142ce8a93faf60f3f42c2bf5670efbc9715e
GerrardXiong <chengcheng.xiong@my.jcu.edu.au>
2022-04-27 16:04:33 +1000

    Passing data to server

--- app.js
+++ app.js
@@ -2,42 +2,42 @@ const express = require("express");
 
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+app.use(express.urlencoded({
+  extended: true
+}));
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Staurday";
-      break;
-    default:
-      console.log("Error: current day is equal to:" + currentDay);
-  }
+app.get("/", (req, res) => {
+
+  var today = new Date();
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
   res.render("list", {
-    kindOfDay: day
+    kindOfDay: day,
+    newListItems: items
   });
+
 });
+
+app.post("/", (req, res) => {
+  var item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
+
+});
+
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
