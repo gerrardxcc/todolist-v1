e00c2461fce4108e6182f81afcd117351c9a1186
GerrardXiong <chengcheng.xiong@my.jcu.edu.au>
2022-04-28 20:49:30 +1000

    add css to todolist and create new page called work

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const express = require("express");
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({
   extended: true
 }));
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
 
@@ -23,7 +26,7 @@ app.get("/", (req, res) => {
   let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items
   });
 
@@ -38,6 +41,15 @@ app.post("/", (req, res) => {
 
 });
 
+app.get("/work", (req, res)=>{
+  res.render("list",{listTitle:"Work List", newListItems: workItems});
+});
+
+app.post("/work", (req, res)=>{
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
