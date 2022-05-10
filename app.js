db633c1679a735b5edc76cb92926ce0d9ae4549e
GerrardXiong <chengcheng.xiong@my.jcu.edu.au>
2022-05-10 17:36:25 +1000

    change font size

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(3001, () => {
+  console.log("Server started on port 3001");
 })
